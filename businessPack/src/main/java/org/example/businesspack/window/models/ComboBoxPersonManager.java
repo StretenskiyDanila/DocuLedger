00859d34402c58688b97249103e6d2e6b2358dd4
TheMadman <stretenskijdanila@gmail.com>
2024-12-23 08:45:43 +0300

    Fix. Добавлен converter в comboBox

--- businessPack/src/main/java/org/example/businesspack/window/models/ComboBoxPersonManager.java
+++ businessPack/src/main/java/org/example/businesspack/window/models/ComboBoxPersonManager.java
@@ -8,8 +8,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
+import javafx.util.StringConverter;
 
-public class ComboBoxManager {
+public class ComboBoxPersonManager {
 
     public static void configureComboBox(ComboBox<PersonDto> comboBox, PersonRole role, PersonService service) {
         comboBox.setEditable(true);
@@ -39,6 +40,21 @@ public class ComboBoxManager {
 
         ObservableList<PersonDto> items = FXCollections.observableArrayList(service.get(role));
         comboBox.setItems(items);
+
+        comboBox.setConverter(new StringConverter<PersonDto>() {
+            @Override
+            public String toString(PersonDto person) {
+                return person == null ? null : person.getName();
+            }
+
+            @Override
+            public PersonDto fromString(String string) {
+                return items.stream()
+                .filter(person -> person.getName().equals(string))
+                .findFirst()
+                .orElse(null);
+            }
+        });
 
         comboBox.getEditor().textProperty().addListener((observable, oldValue, newValue) -> {
             var list = items.filtered(option -> option.getName().toLowerCase().contains(newValue.toLowerCase()));
